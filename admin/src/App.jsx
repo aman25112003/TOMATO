98ac35ae47660a7209c1833db42dfa1b283ef772
AMAN SINGH <amansingh20032511@gmail.com>
2024-07-31 16:25:02 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,7 +8,7 @@ import Orders from './Pages/Orders/Orders';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
-  const url = "http://localhost:4000";
+  const url = "https://tomato-admin-hu85.onrender.com";
 
   return (
     <div>
